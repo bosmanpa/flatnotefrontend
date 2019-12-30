4b4599146b4799a7f38261071bbba675b22df6f7
bosmanpa <bsmn.ptrck@gmail.com>
2019-12-30 11:22:39 -0600

    started nav bar

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import NavBar from '../components/NavBar';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
